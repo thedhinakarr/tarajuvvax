3ea0c4aa6ce2932608f6f9cb6a92d920f46fff6f
thedhinakarr <codedhinakarr@gmail.com>
2025-04-11 10:30:57 +0200

    FIRST SHIT

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,7 @@ const Layout = ({ children }) => {
             fontSize: `var(--font-sm)`,
           }}
         >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+
         </footer>
       </div>
     </>
